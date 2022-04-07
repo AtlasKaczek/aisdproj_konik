29fba6b7b0ff1e7060e5edfa9aec6703656b0f5c
AtlasKaczek <stan.kryj@gmail.com>
2022-04-07 21:50:27 +0200

    Dodano ruch konika

--- main.c
+++ main.c
@@ -51,6 +51,102 @@ void ustawNaSzachownicy(int* szachownica[], int x, int y) {
     szachownica[y][x] = -1;
 }
 
+int ruch(int* szachownica[], int wielkosc, int x, int y, int nx, int ny, int wariant) {
+    
+    switch (wariant)
+    {
+    case 0:
+        nx = x+1;
+        ny = y+2;
+        break;
+    case 1:
+        nx = x+2;
+        ny = y+1;
+        break;
+    case 2:
+        nx = x+2;
+        ny = y-1;
+        break;
+    case 3:
+        nx = x+1;
+        ny = y-2;
+        break;
+    case 4:
+        nx = x-1;
+        ny = y-2;
+        break;
+    case 5:
+        nx = x-2;
+        ny = y-1;
+        break;
+    case 6:
+        nx = x-2;
+        ny = y+1;
+        break;
+    case 7:
+        nx = x-1;
+        ny = y+2;
+        break;
+    }
+    if(nx>=0 && nx<wielkosc && ny>=0 && ny<wielkosc && szachownica[nx][ny]==0) {
+        return 0; // Prawda
+    }
+    return 1; // Falsz
+}
+
+int wykonaj(int* szachownica[], int x, int y, int *nx, int *ny, int wariant) {
+    switch (wariant)
+    {
+    case 0:
+        *nx = x+1;
+        *ny = y+2;
+        break;
+    case 1:
+        *nx = x+2;
+        *ny = y+1;
+        break;
+    case 2:
+        *nx = x+2;
+        *ny = y-1;
+        break;
+    case 3:
+        *nx = x+1;
+        *ny = y-2;
+        break;
+    case 4:
+        *nx = x-1;
+        *ny = y-2;
+        break;
+    case 5:
+        *nx = x-2;
+        *ny = y-1;
+        break;
+    case 6:
+        *nx = x-2;
+        *ny = y+1;
+        break;
+    case 7:
+        *nx = x-1;
+        *ny = y+2;
+        break;
+    }
+    return 0;
+}
+
+void ustawPozycje(int* szachownica[], int wielkosc, int *x, int *y, int ktory, int wariant) {
+    int nx, ny;
+
+    if (ruch(szachownica, wielkosc, *x, *y, nx, ny, wariant) == 0)
+    {  
+        szachownica[*y][*x] = ktory;
+        wykonaj(szachownica, *x, *y, &nx, &ny, wariant);
+        ustawNaSzachownicy(szachownica, nx, ny);
+        wyswietlSzachownice(szachownica, wielkosc);
+    } else {
+        printf("Ruch niemozliwy!\n\n");
+    }
+}
+
 int main()
 {
     // SZACHOWNICA
@@ -74,5 +170,8 @@ int main()
     ustawNaSzachownicy(szachownicaWSK, x, y);
 
     wyswietlSzachownice(szachownicaWSK, wielkosc);
+
+    ustawPozycje(szachownicaWSK, wielkosc, &x, &y, 1, 2);
+
     return 0;
 }

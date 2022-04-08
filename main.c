0b50d8d66aea425a80b343e0fb94d32bb0376786
TheFilips <filip.skibinski@o2.pl>
2022-04-09 00:17:45 +0200

    dodano komentarze

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 
-// FUNCKJE SZACHOWNICA
+// FUNCKJE SZACHOWNICA: Wyświetla planszę do gry
 void wyswietlSzachownice(int* szachownica[], int wielkosc) {
-    printf("Szachownica (%d na %d): \n\n", wielkosc, wielkosc);
-    for (int i = 0; i < wielkosc; i++)
-    {
+    printf("Szachownica (%d na %d): \n\n", wielkosc, wielkosc); // wyświetlenie szachownicy
+    for (int i = 0; i < wielkosc; i++)  // petla tworza szachownice o wielkosci podanej przez uzytkownika
+        {
         for (int j = 0; j < wielkosc; j++)
         {
             if(szachownica[i][j] == -1) {
@@ -19,7 +19,7 @@ void wyswietlSzachownice(int* szachownica[], int wielkosc) {
     }
     printf("\n");
 }
-
+// funckja zeruje wszystkie miejsca szachownicy: plansza jest pust. 
 void wyczyscSzachownice(int* szachownica[], int wielkosc) {
     for (int i = 0; i < wielkosc; i++)
     {
@@ -31,125 +31,125 @@ void wyczyscSzachownice(int* szachownica[], int wielkosc) {
     
 }
 
-// FUNKCJE KONIK
+// FUNKCJE KONIK: Funkcja odpowiadająca za wczytanie pozycji konika podanej przez użytkownika
 void wpiszPozycje(int *x, int *y, int wielkosc) {
     do {
-        printf("Podaj pozycje konika\nx: ");
-        scanf(" %d", x);
-        printf("y: ");
+        printf("Podaj pozycje konika\nx: "); //wyswietla zapytanie dotyczące miejsca startu konika
+        scanf(" %d", x);             // wczytanie pozycji podanej przez użytkownika
+        printf("y: ");                  
         scanf(" %d", y);
-        if (!(*x>=0 && *x<wielkosc && *y>=0 && *y<wielkosc))
+        if (!(*x>=0 && *x<wielkosc && *y>=0 && *y<wielkosc))    // wyswietleneie bledu z braku mozliwosci ruchu 
         {
-            printf("Poza szachownica!\n");
+            printf("Poza szachownica!\n");  
         }
         
     } while (!(*x>=0 && *x<wielkosc && *y>=0 && *y<wielkosc));
     printf("\n");
 }
-
+// fuknkcja wybraną pozycje na szachwoniy
 void ustawNaSzachownicy(int* szachownica[], int x, int y) {
     szachownica[y][x] = -1;
 }
-
+// funckajca sprawdza czy ruch konika jest możliwy i zgodny z zasadmi.
 int sprawdzRuch(int* szachownica[], int wielkosc, int x, int y, int nx, int ny, int wariant) {
     
     switch (wariant)
     {
     case 0:
-        nx = x+1;
-        ny = y+2;
+        nx = x+1;     // sprawdza czy ruch w prawo jest mozliwy 
+        ny = y+2;       // sprawdza czy dwa ruchu do gory sa mozliwe
         break;
     case 1:
-        nx = x+2;
-        ny = y+1;
+        nx = x+2;   // sprawda czy dwa ruchy w prawo sa mozliwe 
+        ny = y+1;   // sprawdza czy ruch do gory jest mozliwy 
         break;
     case 2:
-        nx = x+2;
-        ny = y-1;
+        nx = x+2;   // sprawdza czy dwa ruchy w prawo sa mozliwe
+        ny = y-1;   // sprawdza czy ruch w dol jest mozliwy 
         break;
     case 3:
-        nx = x+1;
-        ny = y-2;
+        nx = x+1; // sprawdza czy ruch w prawo jest mozliwy 
+        ny = y-2;  // sprawdza czy dwa ruchy w dol sa molziwe
         break;
     case 4:
-        nx = x-1;
-        ny = y-2;
+        nx = x-1;   // sprawdza czy ruch w lewo jest mozliwy
+        ny = y-2;  // sprawdza czy dwa ruchy w dol sa mozliwe
         break;
     case 5:
-        nx = x-2;
-        ny = y-1;
+        nx = x-2; // sprawdza czy dwa ruchy w lewo sa mozliwe
+        ny = y-1;   // sprawdza czy ruch w dol jest mozliwy 
         break;
     case 6:
-        nx = x-2;
-        ny = y+1;
+        nx = x-2; //sprawdza czy dwa ruchy w lewo sa mozliwe
+        ny = y+1; // sprawda czy ruch w gore jest mozliwy
         break;
     case 7:
-        nx = x-1;
-        ny = y+2;
-        break;
+        nx = x-1; //sprawdza czy ruch w lewo jest mozliwy 
+        ny = y+2; // sprawdza czy dwa ruchy w gore sa mozlwie
+        break; 
     }
     if(nx>=0 && nx<wielkosc && ny>=0 && ny<wielkosc && szachownica[nx][ny]==0) {
         return 0; // Prawda
     }
     return 1; // Falsz
 }
-
+// funkcja rozpatrująca wszystie mozliwosci poruszania się konika szchowego po szchownicy.
 int wykonajRuch(int* szachownica[], int x, int y, int *nx, int *ny, int wariant) {
     switch (wariant)
     {
     case 0:
-        *nx = x+1;
-        *ny = y+2;
+        *nx = x+1; // przesuniecie o pozycje w prawo 
+        *ny = y+2; // przesuniecie o dwie pozycje do gory
         break;
     case 1:
-        *nx = x+2;
-        *ny = y+1;
+        *nx = x+2; // przesuniecie o dwie pozycje w prawo   
+        *ny = y+1; // przesunicie o dwie pozycje do gory
         break;
     case 2:
-        *nx = x+2;
-        *ny = y-1;
+        *nx = x+2;  // przesuniecie o dwie pozycje w prawo
+        *ny = y-1; // przesuniecie o jedna pozycje w dol
         break;
     case 3:
-        *nx = x+1;
-        *ny = y-2;
+        *nx = x+1;  // przesuniecie o jedna pozycje w prawo
+        *ny = y-2;  // przesuniecie o dwie pozycje w dol
         break;
     case 4:
-        *nx = x-1;
-        *ny = y-2;
+        *nx = x-1;  // przesunicie o jedna pozycje w lewo   
+        *ny = y-2;  // przesuniecie o dwie pozycje w dol
         break;
     case 5:
-        *nx = x-2;
-        *ny = y-1;
+        *nx = x-2; // przesuniecie o dwie pozycje w lewo
+        *ny = y-1; // przesunicie o jedna pozycje w dol 
         break;
     case 6:
-        *nx = x-2;
-        *ny = y+1;
+        *nx = x-2; // przesuniecie o dwie pozycje w lewo
+        *ny = y+1; // przesuniecie o jedna pozycje w gore
         break;
     case 7:
-        *nx = x-1;
-        *ny = y+2;
+        *nx = x-1; // przesuniecie o jedna pozycje w lewo   
+        *ny = y+2; // przesuniecie o dwie pozycje w gore
         break;
     }
     return 0;
 }
-
+// ustawia pozycję konika na szachownicy
 void ustawPozycje(int* szachownica[], int wielkosc, int *x, int *y, int ktory, int wariant) {
     int nx, ny;
 
     if (sprawdzRuch(szachownica, wielkosc, *x, *y, nx, ny, wariant) == 0)
     {  
         szachownica[*y][*x] = ktory;
-        wykonajRuch(szachownica, *x, *y, &nx, &ny, wariant);
-        ustawNaSzachownicy(szachownica, nx, ny);
-        wyswietlSzachownice(szachownica, wielkosc);
+        wykonajRuch(szachownica, *x, *y, &nx, &ny, wariant); // wykonuje posuniecie konika podany przez uzytkownika
+        ustawNaSzachownicy(szachownica, nx, ny); // ustawia na szachownicy konika
+        wyswietlSzachownice(szachownica, wielkosc); // wyswietla szachownice o wielkosci podanej przez uzytkownika
     } else {
-        printf("Ruch niemozliwy!\n\n");
+        printf("Ruch niemozliwy!\n\n"); // pokazanie, ze ustawienie jest niemozliwe do wykonania
     }
 }
 
 int main()
 {
-    // SZACHOWNICA
+    // SZACHOWNICA (wygląd)
     int wielkosc; // wielkosc szachownicy
     printf("Podaj wielkosc szachownicy: ");
     scanf("%d", &wielkosc);
@@ -169,9 +169,9 @@ int main()
     wpiszPozycje(&x,&y, wielkosc);
     ustawNaSzachownicy(szachownicaWSK, x, y);
 
-    wyswietlSzachownice(szachownicaWSK, wielkosc);
+    wyswietlSzachownice(szachownicaWSK, wielkosc);//pokazuje widok szachowncy
     
-    ustawPozycje(szachownicaWSK, wielkosc, &x, &y, 1, 2);
+    ustawPozycje(szachownicaWSK, wielkosc, &x, &y, 1, 2);// ustawienie konika na szchownicy
 
     return 0;
 }
